bb87e8c020ecd37e022c137ef11090d80e50d920
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-11-10 12:21:41 -0500

    code

---
+++ bootstrap-testing/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+//import react
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import nav_bar from './navBar';
+ 
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      { nav_bar() }
+
+  
+
+    
+
+    </div>
+  );
+}
+
+export default App;
