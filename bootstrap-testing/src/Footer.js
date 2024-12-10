cdea6cd10d8f688c4e9113503ae145d671a231bf
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-12-10 14:35:44 -0500

    code

--- bootstrap-testing/src/Footer.js
+++ bootstrap-testing/src/Footer.js
@@ -16,50 +16,73 @@ function Footer() {
 
         <Row id = "contact "className=" text-center p-2">
         <Col className="text-center">
-          <h1 className="bio-font font-color-class heading-responsive-font mt-3" >
+          <h1 className="bio-font font-color-class heading-responsive-font" >
           Contact Me
           </h1>
         </Col>
       </Row>
+
+
+      <Row id = "contact "className=" text-center mt-2 p-2">
+        <Col className="text-center">
+        </Col>
+        <Col className="text-center">
+          <Form className="d-flex">
+            <Form.Control
+              type="Name"
+              placeholder="Name"
+              className="me-2 custom-border form-control custom-placeholder"
+              aria-label="Search"
+            />
+          </Form>
+        </Col>
+        <Col className="text-center">
+        </Col>
+      </Row>
+
+
+      <Row id = "contact "className=" text-center mt-2 p-2">
+        <Col className="text-center">
+        </Col>
+        <Col className="text-center">
+          <Form className="d-flex">
+            <Form.Control
+              type="Email"
+              placeholder="Email"
+              className="me-2 custom-border form-control custom-placeholder"
+              aria-label="Search"
+            />
+          </Form>
+        </Col>
+        <Col className="text-center">
+        </Col>
+      </Row>
+
+      <Row id = "contact "className=" text-center mt-2 p-2">
+        <Col className="text-center">
+        </Col>
+        <Col className="text-center">
+          <Form className="d-flex">
+            <Form.Control
+              type="Message"
+              placeholder="Message"
+              className="me-2 custom-border form-control custom-placeholder"
+              aria-label="Search"
+            />
+          </Form>
+        </Col>
+        <Col className="text-center">
+        </Col>
+      </Row>
+
       <Row id = "contact "className=" text-center p-2">
         <Col className="text-center">
-          <h1 className="bio-font font-color-class heading-responsive-font mt-3" >
-          Contact Me
-          </h1>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2 custom-border form-control custom-placeholder"
-              aria-label="Search"
-            />
-            
-          </Form>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2 custom-border form-control custom-placeholder"
-              aria-label="Search"
-            />
-            
-          </Form>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2 custom-border form-control custom-placeholder"
-              aria-label="Search"
-            />
-            
-          </Form>
-          <Button className=" button-custom-color ">Submit</Button>
+        <Button className=" button-custom-color ">Submit</Button>
+
         </Col>
       </Row>
+
       
-
-
- 
     </Container>
     
 
